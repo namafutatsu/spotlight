d2f3847dffd44d94852bda0a053e05e5c30b6f31
gdrtf <gdrtf@protonmail.com>
2017-05-11 23:40:59 +0200

    Use a mask canvas instead of the circle() method

--- server/image_utils.js
+++ server/image_utils.js
@@ -3,31 +3,12 @@ const Canvas = require('canvas')
 
 const MIN_WINDOW_SIZE = 100  // pixels
 const WINDOW_SIZE_IMG_RATIO = 4  // you need "that N scrolls" to see the whole image
-const INNER_RADIUS_RATIO = 0.7  // the closer to 0, the blurrier the circle bounds.
+const INNER_RADIUS_RATIO = 0.93 // circle radius / shadow radius
+const SHADOW_BLUR = 10 // level of blurring effect
 
 let maxImgDimension = null // in pixels
 let windowSize = null
-let outerRadius = null
-let innerRadius = null
-
-function circle (data) {
-  for (let i = -windowSize / 2; i < windowSize / 2; i += 1) {
-    for (let j = -windowSize / 2; j < windowSize / 2; j += 1) {
-      const d = i * i + j * j
-      if (d > outerRadius) {
-        data[((i + windowSize / 2) * windowSize) * 4 + (j + windowSize / 2) * 4 + 0] = 0
-        data[((i + windowSize / 2) * windowSize) * 4 + (j + windowSize / 2) * 4 + 1] = 0
-        data[((i + windowSize / 2) * windowSize) * 4 + (j + windowSize / 2) * 4 + 2] = 0
-      } else if (d <= outerRadius && d >= (innerRadius)) {
-        // normD is [0, 1]: 1 on outer limit, 0 inner.
-        const normD = ((d - innerRadius) / (outerRadius - innerRadius))
-        data[((i + windowSize / 2) * windowSize) * 4 + (j + windowSize / 2) * 4 + 0] *= (1 - normD)
-        data[((i + windowSize / 2) * windowSize) * 4 + (j + windowSize / 2) * 4 + 1] *= (1 - normD)
-        data[((i + windowSize / 2) * windowSize) * 4 + (j + windowSize / 2) * 4 + 2] *= (1 - normD)
-      }
-    }
-  }
-}
+let mask = null
 
 function computeWindowSize (handicap = 0) {
   windowSize = Math.max(
@@ -35,8 +16,23 @@ function computeWindowSize (handicap = 0) {
     maxImgDimension / WINDOW_SIZE_IMG_RATIO
   )
   windowSize = Math.max(windowSize - handicap, 0)
-  outerRadius = windowSize * windowSize / 4
-  innerRadius = outerRadius * INNER_RADIUS_RATIO
+}
+
+function createMask () {
+  mask = new Canvas(windowSize, windowSize)
+  mask.width = windowSize
+  mask.height = windowSize
+  const maskCtx = mask.getContext('2d')
+  maskCtx.fillStyle = 'black'
+  maskCtx.fillRect(0, 0, mask.width, mask.height)
+  maskCtx.globalCompositeOperation = 'xor'
+  const radius = windowSize / 2
+  const innerRadius = radius * INNER_RADIUS_RATIO
+  maskCtx.arc(radius, radius, innerRadius, 0, 2 * Math.PI)
+  maskCtx.fill()
+  maskCtx.shadowBlur = SHADOW_BLUR
+  maskCtx.shadowColor = 'black'
+  maskCtx.fill()
 }
 
 function getCanvasImageFrom (filePath) {
@@ -45,6 +41,7 @@ function getCanvasImageFrom (filePath) {
   img.src = data
   maxImgDimension = Math.max(img.width, img.height)
   computeWindowSize()
+  createMask()
   return img
 }
 
@@ -61,8 +58,8 @@ function getPixelRegionInImage (img, region) {
   const canvas = new Canvas(region.width, region.height)
   const ctx = canvas.getContext('2d')
   ctx.drawImage(img, -region.left, -region.top)
+  ctx.drawImage(mask, 0, 0)
   const imgData = ctx.getImageData(0, 0, region.width, region.height)
-  circle(imgData.data)  // TODO(vperron): Restore this and opptimize efficiency.
   return imgData.data
 }
 
