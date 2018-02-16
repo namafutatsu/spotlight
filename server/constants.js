c31385ccce8762addd2aad191af760d2caf098cb
Victor Perron <victor@iso3103.net>
2018-02-16 23:49:55 +0100

    Rationalize: split client, server & remove devops

--- server/constants.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  IMAGE_PATH: '/tmp/spotlight_images',
-  MSG_COORDS: 'coords',
-  MSG_INIT: 'init',
-  MSG_REGION: 'region',
-  REFRESH_RATE: 40,  // in milliseconds
-  WEBSOCKET_PORT: 8080
-}
